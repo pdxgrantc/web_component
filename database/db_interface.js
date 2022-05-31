73544acd4975232173bf877aa56e7cfd1c8cbfed
pdxgrantc <pdxgrantc@gmail.com>
2022-05-31 19:31:14 +0000

    production

---
+++ database/db_interface.js
@@ -0,0 +1,83 @@
+const { count } = require('console');
+const fs = require('fs');
+const { exit, title } = require('process');
+
+class Database {
+    constructor() {
+        this.items = [];
+        this.readIn();
+    }
+
+    new_item(new_title, new_day, new_month, new_year, new_description) {
+        var day_created = this.get_current_date();
+ 
+        let new_post = {
+            title: String(new_title),
+            date_created_day: String(day_created.day),
+            date_created_month: String(day_created.month),
+            date_created_year: String(day_created.year),
+            date_due_day: String(new_day),
+            date_due_month : String(new_month),
+            date_due_year: String(new_year),
+            description: String(new_description)
+        };
+        this.items.push(new_post);
+        
+        const file_cache = JSON.stringify(this.items, null, 4);
+        this.writeToFile(file_cache);
+    }
+    
+    get_current_date() {
+        var d = new Date()
+        var new_date = {
+            //keyTime: d.getTime(),
+            year: d.getFullYear(),
+            month: d.getMonth(),
+            day: d.getDate(),
+        }
+        return new_date;
+    }
+
+    create_due_date(new_day, new_month, new_year) {
+        var new_date = {
+            year: new_year,
+            month: new_month,
+            day: new_day
+        }
+        return new_date;
+    }
+
+    writeToFile(file_cache) {
+        fs.writeFileSync(__dirname + '/data/db.json', file_cache);
+    }
+
+    deleteContents() {
+        fs.unlinkSync(__dirname + '/data/db.json');
+    }
+
+    readIn() {
+        let json = require(__dirname + '/data/db.json');
+        this.users = json;
+    }
+
+    delete_item(title) {
+        for (let i = 0; i < this.items.length; i++) {
+            if (this.items[i].title == title) {
+                for (let j = 0; j < this.items[i].posts.length; j++) {
+                    this.items.splice(j, 1);
+                    this.writeToFile();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    get_items() {
+        console.log("Tried to get posts");
+        ary_items = this.items;
+        return ary_items;
+    }
+}
+
+module.exports.Database = Database;
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+var cors = require("cors");
+
+const db = require(__dirname + "/database/db_interface.js")
+const database = new db.Database();
+
+const app = express();
+const port = process.env.PORT || 80;
+
+app.use(cors());
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, './static')));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/new-item', (req, res) => {
+  recieved_items = database.get_items();
+  
+  res.render('new-item', {
+    items: JSON.stringify(recieved_items)
+  });
+});
+
+app.post("/new-item", (request, response) => {
+  //Destructure the request body
+  var resData = {
+    serverData: request.body,
+  };
+
+  database.new_item(request.body.title, request.body.due_day, request.body.due_month, request.body.due_year, request.body.description);
+
+  //Console log the response data (for debugging)
+  //console.log(resData.serverData);
+  //Send the response as JSON with status code 200 (success)
+  response.status(200).render('new-item');
+});
+
+/*
+app.post("/new-item", (req, res) => {
+  let data = req.body;
+  var title = Number(req.body.title);
+  var date = Number(req.body.date);
+  var description = Number(req.body.description);
+  res.redirect("/");
+});
+*/
+app.get('*', (req, res) => { /* 404 error */
+  res.render('404.ejs');
+});
+
+app.listen(port);
+
+console.log("Listening at port http://pdxgrantc.com");
